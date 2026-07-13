bfa30d506f2090c267c8843471bf8b781644b32c
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-07-13 09:47:34 +0200

    Refactor theme context implementation: move AppContext to a new context directory and update imports in Navbar and main files

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-import { ThemeProvider } from "./components/olha/AppContext";
+import { ThemeProvider } from "./context/AppContext";
 import "./index.css";
 
 // Import the generated route tree
